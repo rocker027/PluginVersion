5b8228b772eeab7f90cb59c662f9a73e6d27064b
rocker <rocker@seektopser.com>
2022-09-13 21:26:26 +0800

    feat : first init

---
+++ src/main/java/com/coors/plugin/version/AndroidConfig.kt
@@ -0,0 +1,11 @@
+package com.coors.plugin.version
+
+object AndroidConfig {
+    const val applicationId = "com.coors.demo"
+    const val compileSdkVersion = 32
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 32
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
